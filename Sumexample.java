6308ceb65370c36bf29a3ebcc21e5dedbbf057d3
Sushmitha641 <245122748027@mvsrec.edu.in>
2025-03-18 13:17:50 +0530

    Add files via upload

---
+++ Sumexample.java
@@ -0,0 +1,14 @@
+public class Sumexample {
+    public static void main(String[] args) {
+        // Declare two numbers
+        int num1 = 5;
+        int num2 = 10;
+
+        // Calculate the sum
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
+
